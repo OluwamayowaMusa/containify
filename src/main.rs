8129224240eab5961c332d9f4fe730c0913e5332
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-09-22 01:50:00 +0100

    Testng chroot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("chroot")
+        .arg("testroot")
+        .arg("/hello")
+        .output()
+        .expect("Failed to execute command");
+
+    println!("{:?}", String::from_utf8(output.stdout).unwrap());
 }
